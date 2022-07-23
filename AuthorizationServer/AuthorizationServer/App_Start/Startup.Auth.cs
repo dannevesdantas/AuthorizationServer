56f189d085e46a6f6ea5ae24c7c52c2fae762060
Danilo Dantas <5115895+dannevesdantas@users.noreply.github.com>
2022-07-23 18:22:26 -0300

    Creating AuthorizationServer project

---
+++ AuthorizationServer/AuthorizationServer/App_Start/Startup.Auth.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Infrastructure;
+using AuthorizationServer.Constants;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Linq;
+using AuthorizationServer.Models;
+
+namespace AuthorizationServer
+{
+    public partial class Startup
+    {
+        private readonly ConcurrentDictionary<string, string> _authenticationCodes = new ConcurrentDictionary<string, string>(StringComparer.Ordinal);
+
+        // Para obter mais informações sobre a autenticação de configuração, visite https://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure o contexto db, gerenciador de usuários e gerenciador de login para usar uma única instância por solicitação
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Habilitar o aplicativo a usar um cookie para armazenar informações do usuário logado
+            // e um cookie que armazena informações temporárias sobre um usuário que faz login em um provedor de login de terceiros
+            // Configurar o código de entrada
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Permite que o aplicativo valide o carimbo de segurança quando o usuário efetuar login.
+                    // Este é um recurso de segurança que é usado quando você altera uma senha ou adiciona um login externo à sua conta.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Permite que o aplicativo armazene temporariamente as informações do usuário quando ele estiver verificando o segundo fator no processo de autenticação de dois fatores.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Permite que o aplicativo lembre segundo fator de verificação de login, como telefone ou email.
+            // Assim que você marcar esta opção, sua segunda etapa de verificação durante o processo de login será lembrada no dispositivo no qual você efetuou login.
+            // Isso é semelhante à opção RememberMe (Lembre-me) quando você efetua login.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            //Setup the Authorization Server
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                AuthorizeEndpointPath = new PathString(Paths.AuthorizePath),
+                TokenEndpointPath = new PathString(Paths.TokenPath),
+                ApplicationCanDisplayErrors = true,
+#if DEBUG
+                AllowInsecureHttp = true,
+#endif
+                Provider = new OAuthAuthorizationServerProvider
+                {
+                    OnValidateClientRedirectUri = ValidateClientRedirectUri,
+                    OnValidateClientAuthentication = ValidateClientAuthentication,
+                    OnGrantResourceOwnerCredentials = GrantResourceOwnerCredentials,
+                    OnGrantClientCredentials = GrantClientCredentials
+                },
+
+                // The authorization code provider is the object in charge of creating and receiving
+                // the authorization code.
+                AuthorizationCodeProvider = new AuthenticationTokenProvider
+                {
+                    OnCreate = CreateAuthenticationCode,
+                    OnReceive = ReceiveAuthenticationCode,
+                },
+
+                // The refresh token provider is in charge in creating and receiving refresh 
+                // token.
+                RefreshTokenProvider = new AuthenticationTokenProvider
+                {
+                    OnCreate = CreateRefreshToken,
+                    OnReceive = ReceiveRefreshToken,
+                }
+            });
+
+            //Protect the resources on this server.
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions { });
+
+            // Remover comentário das seguintes linhas para habilitar o logon com provedores de logon de terceiros
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
+
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+        }
+
+        private Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            if (context.ClientId == Clients.Client1.Id)
+            {
+                context.Validated(Clients.Client1.RedirectUrl);
+            }
+            else if (context.ClientId == Clients.Client2.Id)
+            {
+                context.Validated(Clients.Client2.RedirectUrl);
+            }
+            return Task.FromResult(0);
+        }
+
+        private Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId;
+            string clientSecret;
+            if (context.TryGetBasicCredentials(out clientId, out clientSecret) ||
+            context.TryGetFormCredentials(out clientId, out clientSecret))
+            {
+
+                if (clientId == Clients.Client1.Id && clientSecret == Clients.Client1.Secret)
+                {
+                    context.Validated();
+                }
+                else if (clientId == Clients.Client2.Id && clientSecret == Clients.Client2.Secret)
+                {
+                    context.Validated();
+                }
+            }
+            return Task.FromResult(0);
+        }
+
+        private Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(context.UserName, OAuthDefaults.AuthenticationType), context.Scope.Select(x => new Claim("urn:oauth:scope", x)));
+
+            context.Validated(identity);
+
+            return Task.FromResult(0);
+        }
+
+        private Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(new GenericIdentity(context.ClientId, OAuthDefaults.AuthenticationType), context.Scope.Select(x => new Claim("urn:oauth:scope", x)));
+
+            context.Validated(identity);
+
+            return Task.FromResult(0);
+        }
+
+        private void CreateAuthenticationCode(AuthenticationTokenCreateContext context)
+        {
+            context.SetToken(Guid.NewGuid().ToString("n") + Guid.NewGuid().ToString("n"));
+            _authenticationCodes[context.Token] = context.SerializeTicket();
+        }
+
+        private void ReceiveAuthenticationCode(AuthenticationTokenReceiveContext context)
+        {
+            string value;
+            if (_authenticationCodes.TryRemove(context.Token, out value))
+            {
+                context.DeserializeTicket(value);
+            }
+        }
+
+        private void CreateRefreshToken(AuthenticationTokenCreateContext context)
+        {
+            context.SetToken(context.SerializeTicket());
+        }
+
+        private void ReceiveRefreshToken(AuthenticationTokenReceiveContext context)
+        {
+            context.DeserializeTicket(context.Token);
+        }
+
+    }
+}
